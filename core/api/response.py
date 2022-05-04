089032d346dc5edd0b7724ec1d2f0c8302de2bcb
Thomasinio <igorqa.9@gmail.com>
2022-05-05 00:51:37 +0300

    Added type hinting to the Response class

--- core/api/response.py
+++ core/api/response.py
@@ -1,17 +1,20 @@
-from pydantic.main import Model
-
+from typing import TypeVar, Union, List
+from pydantic.main import ModelMetaclass
+from requests import Response as RequestsResponse
 from core.api.enums.global_enums import GlobalErrorMessages
+
+Self = TypeVar('Self', bound='Response')
 
 
 class Response:
 
-    def __init__(self, response):
+    def __init__(self, response: RequestsResponse) -> None:
         self.response = response
         self.response_url = response.url
         self.response_json = response.json()
         self.response_status_code = response.status_code
 
-    def validate(self, model):
+    def validate(self, model: ModelMetaclass) -> Self:
         if isinstance(self.response_json, list):
             for item in self.response_json:
                 parsed_object = model.parse_obj(item)
@@ -21,14 +24,14 @@ class Response:
             self.__extend_object_attributes_with(parsed_object)
         return self
 
-    def assert_status_code(self, status_code):
+    def assert_status_code(self, status_code: Union[int, List[int]]) -> Self:
         if isinstance(status_code, list):
             assert self.response_status_code in status_code, GlobalErrorMessages.WRONG_STATUS_CODE.value
         else:
             assert self.response_status_code == status_code, GlobalErrorMessages.WRONG_STATUS_CODE.value
         return self
 
-    def __extend_object_attributes_with(self, parsed_object: Model) -> None:
+    def __extend_object_attributes_with(self, parsed_object: ModelMetaclass) -> None:
         """
         The method provides attribute style access to a response body.
         Instead of:
@@ -38,5 +41,5 @@ class Response:
         """
         self.__dict__.update(parsed_object.dict())
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"Response({self.response_url}, {self.response_status_code}, {self.response_json})"
