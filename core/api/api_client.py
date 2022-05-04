a654e6db0041031259a97efb1b8ee159f46e9fb5
Thomasinio <igorqa.9@gmail.com>
2022-05-04 23:53:12 +0300

    Added new hook for checking status code

--- core/api/api_client.py
+++ core/api/api_client.py
@@ -7,6 +7,10 @@ from configuration import SERVICE_URL
 from .response import Response
 
 
+def check_for_error(response: Request, *args, **kwargs):
+    response.raise_for_status()
+
+
 def response_logging_hook(response: Request, *args, **kwargs):
     logger.debug(response.url)
     logger.debug(response.status_code)
@@ -15,22 +19,18 @@ def response_logging_hook(response: Request, *args, **kwargs):
 
 class ApiClient:
 
-    def __init__(self, base_url=SERVICE_URL, raise_for_status=True):
+    def __init__(self, base_url=SERVICE_URL):
         self.base_url = furl(base_url)
         self.session = Session()
-        self.session.hooks["response"] = response_logging_hook
-        self.raise_for_status = raise_for_status
+        # list of functions that are called after every response
+        self.session.hooks["response"] = [check_for_error, response_logging_hook]
 
     def get(self, path="/", params=None):
         url = self.base_url / path
         response = self.session.get(url, params=params)
-        if self.raise_for_status:
-            response.raise_for_status()
         return Response(response)
 
     def post(self, path="/", data=None, json=None):
         url = self.base_url / path
         response = self.session.post(url, data, json)
-        if self.raise_for_status:
-            response.raise_for_status()
         return Response(response)
--- core/api/response.py
+++ core/api/response.py
@@ -1,3 +1,5 @@
+from pydantic.main import Model
+
 from core.api.enums.global_enums import GlobalErrorMessages
 
 
@@ -13,10 +15,10 @@ class Response:
         if isinstance(self.response_json, list):
             for item in self.response_json:
                 parsed_object = model.parse_obj(item)
-                self.__convert(parsed_object)
+                self.__extend_object_attributes_with(parsed_object)
         else:
             parsed_object = model.parse_obj(self.response_json)
-            self.__convert(parsed_object)
+            self.__extend_object_attributes_with(parsed_object)
         return self
 
     def assert_status_code(self, status_code):
@@ -26,7 +28,14 @@ class Response:
             assert self.response_status_code == status_code, GlobalErrorMessages.WRONG_STATUS_CODE.value
         return self
 
-    def __convert(self, parsed_object):
+    def __extend_object_attributes_with(self, parsed_object: Model) -> None:
+        """
+        The method provides attribute style access to a response body.
+        Instead of:
+        >>> self.response_json["books"][-1]["name"]
+        You can simply write:
+        >>> self.response_json.books[-1].name
+        """
         self.__dict__.update(parsed_object.dict())
 
     def __repr__(self):
