4863d4b32f6a828e8bbdf12c4695ce3ee77e77cf
Thomasinio <Thomasinio@users.noreply.github.com>
2022-05-05 13:39:12 +0300

    Added a few type hints to the ApiClient class

--- core/api/api_client.py
+++ core/api/api_client.py
@@ -1,36 +1,39 @@
 from furl import furl
 from loguru import logger
 from pprint import pformat
-from requests import Session, Request
+from requests import Session, Response as RequestsResponse
 
-from configuration import SERVICE_URL
+from urls import SERVICE_URL
 from .response import Response
 
 
-def check_for_error(response: Request, *args, **kwargs):
+def check_for_error(response: RequestsResponse, *args, **kwargs) -> None:
     response.raise_for_status()
 
 
-def response_logging_hook(response: Request, *args, **kwargs):
+def response_logging_hook(response: RequestsResponse, *args, **kwargs) -> None:
     logger.debug(response.url)
     logger.debug(response.status_code)
     logger.debug("\n" + pformat(response.json()))
 
 
 class ApiClient:
-
-    def __init__(self, base_url=SERVICE_URL):
+    """
+    Simplified API client
+    Initialized with the base URL to which requests will go
+    """
+    def __init__(self, base_url: str = SERVICE_URL) -> None:
         self.base_url = furl(base_url)
         self.session = Session()
         # list of functions that are called after every response
         self.session.hooks["response"] = [check_for_error, response_logging_hook]
 
-    def get(self, path="/", params=None):
+    def get(self, path: str = "/", params=None) -> Response:
         url = self.base_url / path
         response = self.session.get(url, params=params)
         return Response(response)
 
-    def post(self, path="/", data=None, json=None):
+    def post(self, path: str = "/", data=None, json=None) -> Response:
         url = self.base_url / path
         response = self.session.post(url, data, json)
         return Response(response)
--- core/api/response.py
+++ core/api/response.py
@@ -7,12 +7,21 @@ Self = TypeVar('Self', bound='Response')
 
 
 class Response:
+    """
 
+    """
     def __init__(self, response: RequestsResponse) -> None:
         self.response = response
         self.response_url = response.url
         self.response_json = response.json()
         self.response_status_code = response.status_code
+
+    def assert_status_code(self, status_code: Union[int, List[int]]) -> Self:
+        if isinstance(status_code, list):
+            assert self.response_status_code in status_code, GlobalErrorMessages.WRONG_STATUS_CODE.value
+        else:
+            assert self.response_status_code == status_code, GlobalErrorMessages.WRONG_STATUS_CODE.value
+        return self
 
     def validate(self, model: ModelMetaclass) -> Self:
         if isinstance(self.response_json, list):
@@ -24,19 +33,12 @@ class Response:
             self.__extend_object_attributes_with(parsed_object)
         return self
 
-    def assert_status_code(self, status_code: Union[int, List[int]]) -> Self:
-        if isinstance(status_code, list):
-            assert self.response_status_code in status_code, GlobalErrorMessages.WRONG_STATUS_CODE.value
-        else:
-            assert self.response_status_code == status_code, GlobalErrorMessages.WRONG_STATUS_CODE.value
-        return self
-
     def __extend_object_attributes_with(self, parsed_object: ModelMetaclass) -> None:
         """
-        The method provides attribute style access to a response body.
-        Instead of:
+        The method provides attribute style access to a response body
+        Instead of annoying:
         >>> self.response_json["books"][-1]["name"]
-        You can simply write:
+        We can simply write:
         >>> self.response_json.books[-1].name
         """
         self.__dict__.update(parsed_object.dict())
