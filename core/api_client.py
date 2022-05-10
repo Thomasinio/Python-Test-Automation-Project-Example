01c340c4360562157aa52d6917d57fca2e73462e
Thomasinio <Thomasinio@users.noreply.github.com>
2022-05-10 20:22:24 +0300

    Removed needless type hints

--- core/api_client.py
+++ core/api_client.py
@@ -1,5 +1,3 @@
-from typing import TypeVar, Optional, Any, Dict
-
 from furl import furl
 from loguru import logger
 from pprint import pformat
@@ -10,15 +8,13 @@ from .response_validator import ResponseValidator
 from .models.auth import AuthDataModel, AuthResponseModel
 from core.configs.main_configuration import SERVICE_HOST, AUTH0_HOST, AUTH0_CLIENT_ID
 
-Self = TypeVar('Self', bound='ApiClient')
-
 
 @logger.catch
-def check_for_error(response: Response, *args, **kwargs) -> None:
+def check_for_error(response: Response, *args, **kwargs):
     response.raise_for_status()
 
 
-def response_logging_hook(response: Response, *args, **kwargs) -> None:
+def response_logging_hook(response: Response, *args, **kwargs):
     logger.debug(response.url)
     logger.debug(response.status_code)
     logger.debug("\n" + pformat(response.json()))
@@ -26,26 +22,26 @@ def response_logging_hook(response: Response, *args, **kwargs) -> None:
 
 class ApiClient:
 
-    def __init__(self, base_url: furl = SERVICE_HOST) -> None:
+    def __init__(self, base_url: furl = SERVICE_HOST):
         self.base_url = base_url
         self.session = Session()
         # List of functions that are called after every response
         self.session.hooks["response"] = [check_for_error, response_logging_hook]
         self.games = GameApiClient(client=self)
 
-    def authorize(self, auth_data: AuthDataModel = AuthDataModel()) -> Self:
+    def authorize(self, auth_data: AuthDataModel = AuthDataModel()):
         url = AUTH0_HOST
         token = AuthResponseModel(**self.session.post(url, data=auth_data.dict()).json())
         self.session.headers["Client-ID"] = AUTH0_CLIENT_ID
         self.session.headers["Authorization"] = f"{token.token_type.capitalize()} {token.access_token}"
         return self
 
-    def get(self, path: str = "/", params: Dict[str, Any] = None) -> ResponseValidator:
+    def get(self, path="/", params=None):
         url = self.base_url / path
         response = self.session.get(url, params=params)
         return ResponseValidator(response)
 
-    def post(self, path: str = "/", data: Optional[Any] = None, json: Optional[Any] = None) -> ResponseValidator:
+    def post(self, path="/", data=None, json=None):
         url = self.base_url / path
         response = self.session.post(url, data, json)
         return ResponseValidator(response)
--- core/response_validator.py
+++ core/response_validator.py
@@ -1,30 +1,28 @@
-from typing import TypeVar, Union, List
+from typing import Union, List
 
 from requests import Response
 from pydantic.main import ModelMetaclass
 
 from core.enums.global_enums import GlobalErrorMessages
 
-Self = TypeVar('Self', bound='ResponseValidator')
-
 
 class ResponseValidator:
 
-    def __init__(self, response: Response) -> None:
+    def __init__(self, response: Response):
         self.response = response
         self.response_url = response.url
         self.response_json = response.json()
         self.response_status_code = response.status_code
         self.items = list()
 
-    def assert_status_code(self, status_code: Union[int, List[int]]) -> Self:
+    def assert_status_code(self, status_code: Union[int, List[int]]):
         if isinstance(status_code, list):
             assert self.response_status_code in status_code, GlobalErrorMessages.WRONG_STATUS_CODE.value
         else:
             assert self.response_status_code == status_code, GlobalErrorMessages.WRONG_STATUS_CODE.value
         return self
 
-    def validate(self, model: ModelMetaclass) -> Self:
+    def validate(self, model: ModelMetaclass):
         if isinstance(self.response_json, list):
             for item in self.response_json:
                 parsed_object = model.parse_obj(item)
@@ -34,5 +32,5 @@ class ResponseValidator:
             self.items.append(parsed_object)
         return self
 
-    def __repr__(self) -> str:
+    def __repr__(self):
         return f"Response({self.response_url}, {self.response_status_code}, {self.response_json})"
