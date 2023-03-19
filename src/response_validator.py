b0703b450fe52b08097797935e910549f4bf4195
Thomasinio <Thomasinio@users.noreply.github.com>
2023-03-19 20:47:31 +0200

    Improved readability and maintainability

--- src/response_validator.py
+++ src/response_validator.py
@@ -3,7 +3,7 @@ from typing import Union, List, Dict
 
 from loguru import logger
 from requests import Response
-from pydantic.main import ModelMetaclass, ValidationError
+from pydantic import BaseModel, ValidationError
 
 
 class ResponseValidator:
@@ -11,32 +11,35 @@ class ResponseValidator:
     def __init__(self, response: Response):
         self.response = response
         self.response_url = response.url
-        # response.request.body?
+        self.response_headers = response.headers
         self.response_json = response.json()
         self.response_status_code = response.status_code
         # list() always creates a new object on the heap, but [] can reuse memory cells in many situations
         self.items = list()
 
+    def assert_headers(self, header: str, value: str):
+        assert header in self.response_headers, f"Header {header} not found in response headers: {self.response_headers}"
+        assert self.response_headers[header] == value, f"Header {header} has value: {self.response_headers[header]}, expected: {value}"
+        return self
+
     def assert_status_code(self, status_code: Union[int, List[int]]):
         if isinstance(status_code, list):
-            assert self.response_status_code in status_code
+            assert self.response_status_code in status_code, \
+                f"Expected status code to be one of {status_code}, but got {self.response_status_code}."
         else:
-            assert self.response_status_code == status_code
+            assert self.response_status_code == status_code, \
+                f"Expected status code to be {status_code}, but got {self.response_status_code}."
         return self
 
-    def validate(self, model: ModelMetaclass):
-        if isinstance(self.response_json, list):
-            for item in self.response_json:
-                self._model_parsing_process(model, item)
-        else:
-            self._model_parsing_process(model, self.response_json)
+    def validate(self, model: BaseModel):
+        items = self.response_json if isinstance(self.response_json, list) else [self.response_json]
+        self.items = [self._parse_item(model, item) for item in items]
         return self
 
-    def _model_parsing_process(self, model: ModelMetaclass, item: Union[List, Dict]):
+    @staticmethod
+    def _parse_item(model: BaseModel, item: Union[List, Dict]):
         try:
-            parsed_object = model.parse_obj(item)
-            # annoying
-            self.items.append(parsed_object)
+            return model.parse_obj(item)
         except ValidationError:
             # Loguru will automatically add the traceback of occurring exception while using logger.exception()
             logger.exception("\n" + pformat(item))
