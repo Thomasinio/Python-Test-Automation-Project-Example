7b18dbdc984c72e28e0d5d2b7a0f9df5b20ef0bc
Thomasinio <Thomasinio@users.noreply.github.com>
2023-03-19 18:46:17 +0200

    Constants changed to class-level variables

--- src/game_api_client.py
+++ src/game_api_client.py
@@ -1,14 +1,14 @@
 import allure
 
-games = "/games"
-game_engines = "/game_engines"
-
 
 class GameApiClient:
+
+    games = "/games"
+    game_engines = "/game_engines"
 
     def __init__(self, client):
         self.client = client
 
     @allure.step("Get games information")
-    def get_info(self, query):
-        return self.client.post(path=games, data=query)
+    def get_info(self, search_query):
+        return self.client.post(path=self.games, data=search_query)
